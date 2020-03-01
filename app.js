59a625e6590c70fd3bc14e0dd31e138eff319e8b
yliharsilamikko <Mikko.Yliharsila@seamk.fi>
2020-03-02 01:16:26 +0200

    ws to wss

--- app.js
+++ app.js
@@ -1,24 +1,6 @@
 const axios = require('axios');
 const mqtt = require('mqtt');
-
-const mqtt_host = 'mqtt://hairdresser.cloudmqtt.com';
-const mqtt_options = {
-    clientId: 'computer',
-    port: 26442,
-    host: mqtt_host,
-    username: 'chewglle',
-    password: '0nA9rGfI7F4h',
-    protocol: 'mqtts'
-};
-
-// const test_string = `CURRENT JOINT POSITION:
-// Joint   1:     89.81
-// Joint   2:    -2.58
-// Joint   3:    -14.72
-// Joint   4:    -58.87
-// Joint   5:    -5.81
-// Joint   6:     89.81`;
-
+const mqtt_publisher = mqtt.connect('wss://auoh-mqtt-broker.herokuapp.com');
 
 const main_loop = () => {
     setTimeout(() => {
@@ -40,14 +22,13 @@ const main_loop = () => {
                     joints.push(value);
                 }
                 console.log(start_time_stamp, joints, delta, 'ms');
-                mqtt_client.publish('robot_joints', JSON.stringify(joints));
+                mqtt_publisher.publish('joints', JSON.stringify(joints));
                 main_loop();
             });
     }, 10);
 }
 
-const mqtt_client = mqtt.connect(mqtt_host, mqtt_options);
-mqtt_client.on('connect', () => {
+mqtt_publisher.on('connect', () => {
     console.log('connected to mqtt broker');
     main_loop();
 });
